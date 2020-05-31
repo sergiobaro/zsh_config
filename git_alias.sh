0610c31fb4bdfc89f0483725f58d92bdf99d8deb
Sergio Baró <serbarsim@gmail.com>
2020-05-31 10:27:42 +0200

    First commit

---
+++ git_alias.sh
@@ -0,0 +1,6 @@
+
+alias gbD="git branch -D $1"
+alias gdif="git diff $1"
+alias gdifh="git diff HEAD"
+alias glog="git log --oneline"
+alias gcubr="git rev-parse --abbrev-ref HEAD" # prints current branch
