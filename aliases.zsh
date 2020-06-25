ba5d437fb1a0bd7ac99a73514aa4293434413c23
Sergio Baró <sergio.baro@innocv.com>
2020-06-25 10:58:00 +0200

    Added myip alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# GIT
 alias gbD="git branch -D $1"
 alias gdif="git diff $1"
 alias gdifh="git diff HEAD"
 alias glog="git log --oneline"
 alias gcubr="git rev-parse --abbrev-ref HEAD" # prints current branch
+
+# NETWORK
+alias myip="ipconfig getifaddr en0"
