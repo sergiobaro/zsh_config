d4b26aa0da8e00745f694c24bb0dbaa68883eae2
Sergio Baró <serbarsim@gmail.com>
2020-07-27 18:34:53 +0200

    Added alias for git commit ammend

--- aliases.zsh
+++ aliases.zsh
@@ -5,3 +5,4 @@ alias gdif="git diff $1"
 alias gdifh="git diff HEAD"
 alias glog="git log --oneline"
 alias gcubr="git rev-parse --abbrev-ref HEAD" # prints current branch
+alias gcamd="git commit --amend"
