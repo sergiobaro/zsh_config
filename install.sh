756a86da82f37e6a9334a7ff2ea592a9e6a2b7dd
Sergio Baro <sergio.baro@innocv.com>
2020-08-10 09:41:27 +0200

    Removed unneeded code in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ ohmyzsh_custom_folder=~/.oh-my-zsh/custom
 
 if [ -d "${ohmyzsh_custom_folder}" ]; then
     cp *.zsh "$ohmyzsh_custom_folder"
-    # grep -qxF "$command" "${config_file}" || echo "\n$command" >> "${config_file}"
 else
     echo "${ohmyzsh_custom_folder} not found."
 fi
